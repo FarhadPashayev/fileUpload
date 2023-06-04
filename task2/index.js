1c95c515b5fdec0dc268bb9e6f55b00939efd07c
pasaf <pasaf@FARHAD>
2023-06-04 16:02:24 +0400

    task2 complate

---
+++ task2/index.js
@@ -0,0 +1,23 @@
+async function logJSONData(infoData) {
+    const response = await fetch(infoData);
+    const jsonData = await response.json();
+    printFunc(jsonData);
+  }
+  
+  async function printFunc(jsonData) {
+    let name = document.getElementById("name");
+    let username = document.getElementById("username");
+    let followers_number = document.getElementById("followers_number");
+    let following_number = document.getElementById("following_number");
+    let bio = document.getElementById("bio");
+    let image = document.getElementById("profile_image");
+    image.src = jsonData.avatar_url;
+    name.innerHTML = jsonData.name;
+    username.innerHTML = jsonData.login;
+    followers_number.innerHTML = jsonData.followers;
+    following_number.innerHTML = jsonData.following;
+    bio.innerHTML = jsonData.bio;
+  }
+  
+  let infoData = "https://api.github.com/users/ferhad9999";
+  logJSONData(infoData);
